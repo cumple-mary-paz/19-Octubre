6b612c1afe970739413634da25765a3d9bc11ee5
Alejandro Torres <aleth2k@gmail.com>
2024-08-19 19:59:01 -0600

    balloons

---
+++ docs/balloon_script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const balloons = document.querySelectorAll(".balloon");
+
+  const xPositions = Array.from(
+    { length: balloons.length },
+    (_, i) => i * 8 + 5
+  );
+
+  for (let i = xPositions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [xPositions[i], xPositions[j]] = [xPositions[j], xPositions[i]];
+  }
+
+  balloons.forEach((balloon, index) => {
+    gsap.to(balloon, {
+      y: -window.innerHeight,
+      duration: 10 + index * 2,
+      ease: "power1.inOut",
+      repeat: -1,
+      yoyo: true,
+      delay: index * 0.5,
+    });
+
+    balloon.style.left = `${xPositions[index]}%`;
+  });
+});
